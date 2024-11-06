8d283c407886492c9df9dfb6eec310dba34ec316
Nilton <jornalista.nilton@gmail.com>
2024-11-05 21:43:35 -0300

    index common

---
+++ src/components/common/index.tsx
@@ -0,0 +1,5 @@
+export * from "@/components/common/forms/CreateCategoryForm";
+export * from "@/components/common/forms/EditCategoryForm";
+
+export * from "@/components/common/forms/CreateHeroForm";
+export * from "@/components/common/forms/EditHeroForm";
