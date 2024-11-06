a3bc5aa75ed9e03d609a0db35986a19c94afedd9
Nilton <jornalista.nilton@gmail.com>
2024-11-05 21:41:26 -0300

    config next

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -113,8 +113,6 @@ const config: Config = {
       },
       borderRadius: {
         lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
