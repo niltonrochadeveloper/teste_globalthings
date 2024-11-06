dcb51744912e65a5763dbb874cadd916f675f519
Nilton <jornalista.nilton@gmail.com>
2024-11-05 21:44:40 -0300

    edit category table create

---
+++ src/components/shared/editCategoryModal/index.tsx
@@ -0,0 +1,42 @@
+import EditCategoryForm from "@/components/common/forms/EditCategoryForm";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui";
+import { CategoryProps } from "@/models/types/category";
+import { PencilIcon } from "lucide-react";
+import { useState } from "react";
+
+const editCategoryModal = ({ itemToEdit }: { itemToEdit: CategoryProps }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  return (
+    <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
+      <DialogTrigger asChild className="cursor-pointer">
+        <PencilIcon size={20}>editar</PencilIcon>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Adicionar Categoria</DialogTitle>
+          <DialogDescription>
+            Crie a categoria para seu Super Herói
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-2 py-4">
+          <div className="flex flex-col gap-1">
+            <EditCategoryForm
+              itemToEdit={itemToEdit}
+              open={isModalOpen}
+              onOpenChange={setIsModalOpen}
+            />
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default editCategoryModal;
