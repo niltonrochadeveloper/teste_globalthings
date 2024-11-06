6286b185223d66898eadee4d0f880e4dfb98c569
Nilton <jornalista.nilton@gmail.com>
2024-11-05 21:42:33 -0300

    index shared

---
+++ src/components/shared/index.tsx
@@ -0,0 +1,11 @@
+export * from "@/components/shared/menu";
+export * from "@/components/shared/navbar";
+export * from "@/components/shared/dropdownAction";
+
+export * from "@/components/shared/categoryTable";
+export * from "@/components/shared/editCategoryModal";
+export * from "@/components/shared/deleteCategoryModal";
+
+export * from "@/components/shared/createHero";
+export * from "@/components/shared/editHeroModal";
+export * from "@/components/shared/deleteHeroModal";
